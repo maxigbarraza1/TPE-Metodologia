b721d858d93445d4af91ef47714caa012be66a4b
Maximiliano G. Barraza <49289366+maxibarraza1@users.noreply.github.com>
2019-06-12 17:11:56 -0300

    Funcionalidad vecino

--- source/src/funcionalidades/FuncionalidadEmpMunicipal.java
+++ source/src/funcionalidades/FuncionalidadEmpMunicipal.java
@@ -1,9 +1,79 @@
 package funcionalidades;
 
-public class FuncionalidadEmpMunicipal {
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Hashtable;
 
+import estadisticas.Estadistica;
+import productos.Producto;
+import productos.ProductoRegistrado;
+import ubicaciones.Ubicacion;
+
+public class FuncionalidadEmpMunicipal {
+	
+//Estructura
+	
+//Constructor
 	public FuncionalidadEmpMunicipal() {
 		// TODO Auto-generated constructor stub
 	}
+	
+//Metodos
+	public void cargarHorarios(Calendar inicio,Calendar fin) {
+		
+	}	
+	
+	public ArrayList<Estadistica> getEstadisticasHistoricas(AccesoBaseDatos bDatos){
+		return (bDatos.getEstadisticas());
+	}
+	
+	public Hashtable<Producto,Integer> getEstadisticasGeolocalizadas(){
+		return null;
+	}
+	
+	public ArrayList<Estadistica> getEstadisticasMensuales(AccesoBaseDatos bDatos,Calendar walter ){
+		ArrayList<Estadistica> aux= new ArrayList<Estadistica>();
+		if (bDatos.getEstadisticas().isEmpty()) {
+			return null;
+		}
+		else {
+			
+		}
+		return aux;
+	}
+	
+	public double getVolumenCargaActual() {
+		return 1;
+	}
+	
+	
+	public Hashtable<Producto,Integer> getProductosReciclados(){
+		return null;
+	}
+	
+	public void notificarPLIVacio() {
+		
+	}
+	
+	public void notificarPLILleno() {
+		asd
 
+	}
+	
+	public Ubicacion getUbicacionPL() {
+		return null;
+	}
+	
+	public Ubicacion getUbicacionPLI() {
+		return null;
+	}
+	
+	public ArrayList<Ubicacion> getUbicacionCamiones(){
+		return null;
+	}
+	
+	
+	
+	
 }
